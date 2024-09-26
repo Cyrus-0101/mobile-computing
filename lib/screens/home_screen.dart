1df2a5689984d07c72bc4ad789279d7ac4044c5a
Cyrus-0101 <cyrus_g@outlook.com>
2024-09-26 15:39:28 +0300

    chore: Created our homepage screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app/providers/task_provider.dart';
+import 'package:weather_app/widgets/task_tile.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final tasks = Provider.of<TaskProvider>(context).tasks;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo List App'),
+      ),
+      body: ListView.builder(
+        itemCount: tasks.length,
+        itemBuilder: (context, index) {
+          final task = tasks[index];
+          return TaskTile(
+              title: task.title,
+              isDone: task.isDone,
+              onChanged: (value) {
+                Provider.of<TaskProvider>(context, listen: false)
+                    .toggleTaskStatus(index);
+              });
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).pushNamed('/add-task');
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
